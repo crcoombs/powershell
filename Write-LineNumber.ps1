08f8ef52c28a5b23c06b19e71d6ac07bf87ce665
Cheyne Coombs <cheynecoombs@gmail.com>
2015-06-10 09:08:08 -0400

    Initial commit

---
+++ Write-LineNumber.ps1
@@ -0,0 +1,50 @@
+﻿Function Write-LineNumber
+{
+<#
+.SYNOPSIS
+Prints text to the screen with line numbers.
+
+.DESCRIPTION
+Takes a multiline string and prepends line numbers to each line.
+
+.PARAMETER Object
+The text to be displayed.
+
+.PARAMETER Start
+The number at which to start counting lines. Default is 0.
+
+.INPUTS
+System.String[].
+
+.OUTPUTS
+None.
+
+.EXAMPLE
+C:\PS> Write-LineNumber $foo
+
+.EXAMPLE
+C:\PS> Get-Content .\foo.txt | Write-LineNumber
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+        [Object]$Object,
+
+        [Parameter(Mandatory=$False)]
+        [Int]$Start = 0
+    )
+
+    Begin
+    {
+        $Counter = $Start
+    }
+
+    Process
+    {
+        ForEach($SubObject in $Object)
+        {
+            Write-Host "${Counter}: $SubObject"
+            $Counter += 1
+        }
+    } 
+}
---
+++ Write-Timestamp.ps1
@@ -0,0 +1,46 @@
+﻿Function Write-Timestamp
+{
+<#
+.SYNOPSIS
+Prints text to the screen with a timestamp.
+
+.DESCRIPTION
+Prints text with either the current time or elapsed time.
+
+.PARAMETER Object
+The text to be displayed.
+
+.PARAMETER StartTime
+The time from which to start counting.
+
+.EXAMPLE
+C:\PS> Write-Timestamp $foo (Get-Date)
+
+.EXAMPLE
+C:\PS> Get-Content file.txt | Write-Timestamp
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+        [Object]$Object,
+
+        [Parameter(Mandatory=$False)]
+        [DateTime]$StartTime
+    )
+
+    Begin{}
+
+    Process
+    {
+        If($StartTime)
+        {
+            Write-Host "$((Get-Date).Subtract($StartTime).ToString()): " -ForegroundColor Green -NoNewline
+        }
+        Else
+        {
+            Write-Host "$((Get-Date).TimeOfDay): " -ForegroundColor Green -NoNewline
+        }
+
+        Write-Host $Object
+    }
+}
